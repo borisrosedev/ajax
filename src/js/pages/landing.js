5578129420ea73691a7f32270f97bc1a6a87468f
Boris Rose <boris.alexandre.rose@gmail.com>
2024-06-17 16:35:01 +0200

    feat/fetch

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -7,3 +7,18 @@ landingMain.innerHTML =
   paragraph("Les") +
   paragraph("Amis") +
   button("Valider", "submit", "login-button");
+
+
+
+  fetch('https://chezbobo.onrender.com/products', {
+    method: 'GET'
+  })
+    .then((result) => {
+        return result.json()
+    })
+    .then((javascript) => {
+      console.log('mes données', javascript);
+    })
+
+
+    
